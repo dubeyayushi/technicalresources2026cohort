cbe525f0a76b6858dc4ea083ef47416590f91ee3
Ayushi Dubey <119059967+dubeyayushi@users.noreply.github.com>
2023-01-19 23:20:40 +0530

    Submitting Question 10 of Assignment 1

---
+++ a1_q10.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+
+    //write your code here
+    while (n%2 == 0){
+      cout<<"2"<<" ";
+      n = n/2;
+   }
+   for (int i = 3; i <= sqrt(n); i = i+2){
+      while (n%i == 0){
+         cout<<i<<" ";
+         n = n/i;
+      }
+   }
+   if (n > 2)
+   cout<<n<<" ";
+}
