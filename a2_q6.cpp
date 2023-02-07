7bc1c776790b62c14328c6b8d60030ea02ae344d
Ayushi Dubey <119059967+dubeyayushi@users.noreply.github.com>
2023-02-07 13:56:18 +0530

    Submitting Question 6 of Assignment 2

---
+++ a2_q6.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+
+    //write your code here
+    for (int i = 0; i<= n/2; i++){
+        for (int j = 0; j<=(n/2)-i; j++){
+            cout << "*" << "\t";
+        }
+        for (int k = 0; k<2*(i)+1; k++){
+            cout << "\t";
+        }
+        for (int j = 0; j<=(n/2)-i; j++){
+            cout << "*" << "\t";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i<n/2; i++){
+        for (int j = 0; j<=(i+1); j++){
+            cout << "*" << "\t";
+        }
+        for (int k = (n-2)-(2*i); k>0; k--){
+            cout << "\t";
+        }
+        for (int j = 0; j<=(i+1); j++){
+            cout << "*" << "\t";
+        }
+        cout << endl;
+    }
+}
